53d52287964757059c83f2990f248521bc1d78ae
Yasin Yıldırım <jeffyasin@windowslive.com>
2020-12-27 00:35:44 +0300

    Add Product

--- app/src/main/java/com/yasin/addproduct/AddPro.java
+++ app/src/main/java/com/yasin/addproduct/AddPro.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -15,135 +17,128 @@ import android.provider.OpenableColumns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class AddPro extends AppCompatActivity
 {
-
-    private static final int IMAGE_CODE = 1;
-    Button selectBtn;
-    RecyclerView recyclerView;
-    List<ModClass> modalClassList;
-    CAdapter customAdapter;
-    ImageView MyPhoto;
+    private static final int PICK_IMG = 1;
+    private ArrayList<Uri> ImageList = new ArrayList<Uri>();
+    private int uploads = 0;
+    private DatabaseReference databaseReference;
     private StorageReference mStorageRef;
+    private ProgressDialog progressDialog;
+    int index = 0;
+    TextView textView;
+    Button choose,send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pro);
-        selectBtn = findViewById(R.id.select_btn);
-        recyclerView = findViewById(R.id.recyclerViewId);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         mStorageRef = FirebaseStorage.getInstance().getReference();
-
-        modalClassList = new ArrayList<>();
-
-        selectBtn.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                startActivityForResult(intent, IMAGE_CODE);
-            }
-        });
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("User_one");
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Uploading ..........");
+        textView = findViewById(R.id.text);
+        choose = findViewById(R.id.choose);
+        send = findViewById(R.id.upload);
     }
 
+    public void choose(View view) {
+        //we will pick images
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        startActivityForResult(intent, PICK_IMG);
+
+    }
+
+    @SuppressLint("SetTextI18n")
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
-    {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == IMAGE_CODE && resultCode == RESULT_OK)
+        if (requestCode == PICK_IMG)
         {
-            if (data.getClipData() != null)
+            if (resultCode == RESULT_OK)
             {
-                int totalitem = data.getClipData().getItemCount();
+                if (data.getClipData() != null) {
+                    int count = data.getClipData().getItemCount();
+                    int CurrentImageSelect = 0;
+                    while (CurrentImageSelect < count)
+                    {
+                        Uri imageuri = data.getClipData().getItemAt(CurrentImageSelect).getUri();
+                        ImageList.add(imageuri);
+                        CurrentImageSelect = CurrentImageSelect + 1;
+                    }
+                    textView.setVisibility(View.VISIBLE);
+                    textView.setText("You Have Selected "+ ImageList.size() +" Pictures" );
+                    choose.setVisibility(View.GONE);
+                }
+            }
+        }
+    }
 
-                for (int i = 0; i < totalitem; i++)
+    @SuppressLint("SetTextI18n")
+    public void upload(View view)
+    {
+        textView.setText("Please Wait ... If Uploading takes Too much time please the button again ");
+        progressDialog.show();
+        final StorageReference ImageFolder =  FirebaseStorage.getInstance().getReference().child("ImageFolder");
+        for (uploads=0; uploads < ImageList.size(); uploads++) {
+            Uri Image  = ImageList.get(uploads);
+            final StorageReference imagename = ImageFolder.child("image/"+Image.getLastPathSegment());
+
+            imagename.putFile(ImageList.get(uploads)).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>()
+            {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
                 {
-                    Uri imageUri = data.getClipData().getItemAt(i).getUri();
-                    String imagename = getFileName(imageUri);
-
-                    ModClass modalClass = new ModClass(imagename,imageUri);
-                    modalClassList.add(modalClass);
-
-                    customAdapter = new CAdapter(AddPro.this, modalClassList);
-                    recyclerView.setAdapter(customAdapter);
-
-                    StorageReference mRef = mStorageRef.child("image").child(imagename);
-
-                    mRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>()
+                    imagename.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>()
                     {
                         @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+                        public void onSuccess(Uri uri)
                         {
-
-                            Toast.makeText(AddPro.this, "Done", Toast.LENGTH_SHORT).show();
-
-                        }
-                    }).addOnFailureListener(new OnFailureListener()
-                    {
-                        @Override
-                        public void onFailure(@NonNull Exception e)
-                        {
-                            Toast.makeText(AddPro.this, "Fail" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            String url = String.valueOf(uri);
+                            SendLink(url);
                         }
                     });
                 }
-            }
-            else if (data.getData() != null)
-            {
-                Toast.makeText(this, "single", Toast.LENGTH_SHORT).show();
-            }
-
+            });
         }
-
     }
 
-    public String getFileName(Uri uri)
+    private void SendLink(String url)
     {
-        String result = null;
-        if (uri.getScheme().equals("content"))
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("link", url);
+        databaseReference.push().setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>()
         {
-            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-            try
+            @Override
+            public void onComplete(@NonNull Task<Void> task)
             {
-                if (cursor != null && cursor.moveToFirst())
-                {
-                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
-                }
+                progressDialog.dismiss();
+                textView.setText("Image Uploaded Successfully");
+                send.setVisibility(View.GONE);
+                ImageList.clear();
             }
-            finally
-            {
-                cursor.close();
-            }
-        }
-        if (result == null)
-        {
-            result = uri.getPath();
-            int cut = result.lastIndexOf('/');
-            if (cut != -1)
-            {
-                result = result.substring(cut + 1);
-            }
-        }
-        return result;
+        });
     }
 }
